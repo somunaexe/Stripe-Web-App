f5b57257b4eb3fabbc60e2cec11a114005b13849
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-09-28 22:24:07 +0100

    settings committed

--- obj/Debug/net8.0/StripeWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StripeWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StripeWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d846328b45f14be77eef72c7ad8d103324395f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StripeWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StripeWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
